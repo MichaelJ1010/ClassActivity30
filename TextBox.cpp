9452e592990ad69c7b367bbecbf6b08f975c93f0
Michael J <30031129@cc.peralta.edu>
2024-11-21 17:48:49 -0800

    added explicit constructor for textbox

--- TextBox.cpp
+++ TextBox.cpp
@@ -7,5 +7,8 @@ using namespace std;
 string TextBox::getValue()const {
 	return value;
 }
-
+void TextBox::setValue(string value) {
+	this->value = value;
+	cout << "setting value to " << value << endl;
+}
 

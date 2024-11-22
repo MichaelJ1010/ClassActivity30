d99663289cdb0dddbfdd473552e20274c54ecb07
Michael J <30031129@cc.peralta.edu>
2024-11-21 17:51:36 -0800

    test case for explicit constructor

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 int main() {
 	TextBox test;
+	TextBox test2("world");
+	
 	
 	cout << "test value equal to \"\"? " << boolalpha << (test.getValue() == "") << endl;
 
 	test.setValue("hello");
 	cout << "value for test is: " << test.getValue() << endl;
+
+	cout << "value for test2 is: " << test2.getValue() << endl;
 
 	return 0;
 }
